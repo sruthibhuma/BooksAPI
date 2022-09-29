2c806d9a450a66b3212a2da72acbea8792f9f030
sruthibhuma <bhumasruthi24@gmail.com>
2022-09-29 11:32:21 +0530

    Updated with EF and Migrations

---
+++ Data/Models/Book.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BooksAPI.Data.Models{
+
+    public class Book {
+
+        public int Id { get; set; }
+        public string Name { get; set;}
+        public string Author { get; set; }
+
+    }
+
+
+
+}
