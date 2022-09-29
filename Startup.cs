4ed4d897cf3ef2be13f984fff6786ba4b41b25e8
sruthibhuma <bhumasruthi24@gmail.com>
2022-09-29 14:35:22 +0530

    Include Startup.cs

---
+++ Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using BooksAPI.Data;
+
+namespace BooksAPI
+{
+    public class Startup
+    {
+        public string ConnectionString { get; set; }
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("connectionString");
+        }
+
+        //Add Services
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            //Configure DBContext
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(ConnectionString));
+            
+            //Configure Services 
+           //TODO: services.AddTransient<BooksService>();
+           services.AddSwaggerGen();
+        //    services.AddSwaggerGen( c =>
+        //     {
+        //         c.SwaggerDoc("v1", new OpenApiInfo { Title = "BooksAPI", Version = "v1" });
+        //     });
+
+        }
+
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BooksAPI"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //Authentication & Authorization
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            //Exception Handling
+           // app.ConfigureBuildInExceptionHandler(loggerFactory);
+            //app.ConfigureCustomExceptionHandler();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            //AppDbInitializer.Seed(app);
+            AppDbInitializer.Seed(app);
+        }
+    }
+}
